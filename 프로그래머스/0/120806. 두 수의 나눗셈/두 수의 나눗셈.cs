40002b602153cf8eee8119ad5557825628cd6875
혜성 <nalbajo@naver.com>
2025-03-20 16:41:14 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.19 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public int solution(float num1, float num2) {
+        float i = (num1/num2)*1000;
+        int answer = (int)i ;
+        return answer;
+    }
+}
